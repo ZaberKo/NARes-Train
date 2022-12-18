c400ea2b15fc3ea0af899fa29b0a8c8964226ab5
shihuahuang <shihuahuang95@gmail.com>
2022-12-18 17:16:52 +0800

    update

--- train_scripts/robust_cifar10_random.sh
+++ train_scripts/robust_cifar10_random.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=4 nohup python main.py --config_path ./configs/random_ideas --version WRN-28-10-moo  > train_logs/random_ideas/WRN-28-10-moo.log &
+CUDA_VISIBLE_DEVICES=4 python main.py --config_path ./configs/random_ideas --version WRN-28-10-moo  > train_logs/random_ideas/WRN-28-10-moo.log &
